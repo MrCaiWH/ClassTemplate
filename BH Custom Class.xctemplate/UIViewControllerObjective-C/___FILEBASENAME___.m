e43d95755f1734772014b2bdb6859758d27aca55
黄花菜 <18811108252@163.com>
2018-08-14 10:50:09 +0800

    Update ___FILEBASENAME___.m
    1

--- BH Custom Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
+++ BH Custom Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -19,36 +19,21 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
-}
-/*
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-}
-
--(void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
-}
-**/
-- (void)dealloc {
-    NSLog(@"%@-释放了",self.class);
+    self.navigationItem.title = @"";
+    
+    [self setup];
 }
 
 #pragma mark - Private Method
+- (void)setup {
 
-#pragma mark - External Delegate
-
-#pragma mark - Intial Methods
-
-#pragma mark - Lazy
+}
 
 #pragma mark - Target Methods
 
+#pragma mark - Lazy
+
+- (void)dealloc {
+    NSLog(@"%@-释放了",self.class);
+}
 @end
