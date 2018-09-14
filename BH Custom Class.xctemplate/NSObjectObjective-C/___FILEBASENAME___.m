c3bf7edc7633a24aa6abf62a4312d78e24f20818
蔡万鸿 <caiwanhong@koolearn.com>
2018-09-14 10:23:01 +0800

    add nsobject模板

---
+++ BH Custom Class.xctemplate/NSObjectObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,19 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Intial Methods
+
+#pragma mark - Lazy
+
+- (void)dealloc {
+    NSLog(@"%@-释放了",self.class);
+}
+
+@end
